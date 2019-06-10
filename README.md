77ee7ceb14dfde25d10de326f4d13911da03ea72
HH964 <49995995+HH964@users.noreply.github.com>
2019-06-09 20:45:22 -0500

    updated readme, got js to change values of html, got js to not change value every time key pressed

--- README.md
+++ README.md
@@ -1,3 +1,3 @@
 # Psychic-Game
 Psychic Game hw
-In this game you will try guessing the letter(s) that the computer is guessing. The keys you can use are the one in the english alphabet. This game will count wins and losses. It will also show you which letters you have guessed so far.
+In this game you will try guessing the letter(s) that the computer is guessing. The keys you can use are the one in the english alphabet. This game will count wins and losses. It will also show you which letters you have guessed so far (up to your 9th guess per round). To restart the game refresh the page.
--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -9,13 +9,30 @@ document.onkeydown= function (event){
         var compGuess= compAlpha[Math.floor(Math.random() * compAlpha.length)];
             var choice=["a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","w","x","y","z",];
 
-    if (choice.includes(userInput) > -1){
+    // console.log(userInput);
+    // console.log(choicesGuessed);
+    // console.log(numofWins);
+    // console.log(numofLosses);
+    console.log(guessesLeft);
+    // console.log();
+    if (choice.indexOf(userInput) > -1){
         
         if (userInput === compGuess){
             numofWins++;
             choicesGuessed=[];
             guessesLeft= 9;
-            console.log(userInput);
+        }
+        else{
+            if (choicesGuessed.includes(userInput)){
+                var isSameKey= false;
+                for (var i=0; i<userInput.length; i++){
+                    if(userInput != userInput[i]){
+                        isSameKey= true;
+                        break;
+                    }
+                }
+            }
+
         }
 
         if (guessesLeft === 0){
@@ -28,5 +45,17 @@ document.onkeydown= function (event){
             guessesLeft--;
             choicesGuessed.push(userInput);
         }
+
+        var html =
+            "<h1><strong>The Psychic Games</strong></h1>" +
+        
+            "<hr />" +
+            
+            "<h3>Guess what letter I'm thinking of</h3>" +
+            "<h3>Total number of Wins: " + numofWins +"</h3>" +
+            "<h3>Total number of Loses: " + numofLosses + "</h3>" +
+            "<h3>Number of guesses left: " + guessesLeft + "</h3>" +
+            "<h3>Letters guessed so far: " + choicesGuessed.join(",") + "</h3>"
+            document.querySelector("#textChange").innerHTML = html;
     }
 }
